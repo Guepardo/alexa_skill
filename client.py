f440f7a52dca923f4b5d774246fd2067feae52e3
Allyson Maciel Guimarães <bsinet@hotmail.com>
2022-12-01 23:22:55 -0300

    Chore: add client class template

---
+++ client.py
@@ -0,0 +1,6 @@
+class Client:
+    def __init__():
+        pass
+
+    def run():
+        print("Olar")
--- main.py
+++ main.py
@@ -1,4 +1,15 @@
+import sys
 from server import Server
+from client import Client
 
 server = Server()
 server.run()
+
+start_mode = sys.agv[1]
+
+if "__main__" == __name__:
+    if start_mode == 'server':
+        Server().run()
+
+    if start_mode == 'client':
+        Client().run()
