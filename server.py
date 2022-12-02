a88f1797c9cc631867757d91c35911feeb5f1741
Allyson Maciel Guimarães <bsinet@hotmail.com>
2022-12-01 23:18:36 -0300

    Chore: create server class

--- server.py
+++ server.py
@@ -1,4 +1,35 @@
-from server import Server
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
 
-server = Server()
-server.run()
+class Server:
+    def __init__(self):
+      self.app = Flask(__name__)
+      self.app.config['SECRET_KEY'] = 'secret!'
+      self.socketio = SocketIO(self.app)
+
+      self.register_socket_events()
+      self.register_views()
+
+    def register_views(self):
+        self.app.add_url_rule("/", "index", lambda: render_template("index.html"))
+
+    def register_socket_events(self):
+        self.socketio.on_event('my_event', self.on_my_event)
+        self.socketio.on_event('my_broadcast_event', self.on_broadcast_event)
+        self.socketio.on_event('connect', self.on_connect)
+        self.socketio.on_event('disconnect', self.on_disconnect)
+
+    def on_my_event(self, message):
+      emit('my_response', {'data': message['data']})
+
+    def on_broadcast_event(self, message):
+      emit('my_response', {'data': message['data']}, broadcast=True)
+
+    def on_connect(self, message):
+      emit('my_response', {'data': 'Connected'})
+
+    def on_disconnect(self, message):
+      print('Client disconnected')
+
+    def run(self):
+        self.socketio.run(self.app, host='0.0.0.0')
